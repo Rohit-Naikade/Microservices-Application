a8aee69b953736540acf87dddccbca2a3ef81dcc
Rohit-Naikade <naikaderohit833@gmail.com>
2025-03-06 23:26:06 +0530

    06.03.25

--- order-service/src/main/java/com/microservices/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/microservices/order_service/config/RestClientConfig.java
@@ -1,13 +1,18 @@
 package com.microservices.order_service.config;
 
 import com.microservices.order_service.client.InventoryClient;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.sql.Time;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,11 +23,26 @@ public class RestClientConfig {
     @Bean
     public InventoryClient inventoryClient(){
         RestClient restClient= RestClient.builder()
-                .baseUrl(inventoryServiceUrl).build();
+                .baseUrl(inventoryServiceUrl)
+                //.requestFactory(getClientRequestFactory())
+                .build();
 
         var restClientAdapter= RestClientAdapter.create(restClient);
 
         var httpServiceProxyFactory= HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+//    private ClientHttpRequestFactory getClientRequestFactory() {
+//        RequestConfig config = RequestConfig.custom()
+//                .setConnectTimeout(Timeout.ofSeconds(3)) // Connection timeout
+//                .setResponseTimeout(Timeout.ofSeconds(3))   // Read timeout
+//                .build();
+//
+//        CloseableHttpClient client = HttpClients.custom()
+//                .setDefaultRequestConfig(config)
+//                .build();
+//
+//        return new HttpComponentsClientHttpRequestFactory(client);
+//    }
 }
